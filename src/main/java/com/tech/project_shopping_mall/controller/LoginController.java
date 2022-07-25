5a715ad5b1685160fc073aaf32bc213fdb5a2c1e
suj9810 <suj9810@naver.com>
2022-07-25 11:21:20 +0900

    ej test

---
+++ src/main/java/com/tech/project_shopping_mall/controller/LoginController.java
@@ -0,0 +1,35 @@
+package com.tech.project_shopping_mall.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login/*")
+public class LoginController {
+	
+//	@RequestMapping("/login")
+//	public String login() {
+//		System.out.println("login");
+//		
+//		return "login";
+//	}
+	@RequestMapping("/loginform")
+	public String loginform(HttpServletRequest request) {
+//		System.out.println("login");
+//		
+//		System.out.println("카카오로그인");
+//		System.out.println(request.getParameter("kakaoemail"));
+//		System.out.println(request.getParameter("kakaoname"));
+//		System.out.println(request.getParameter("kakaobirth"));
+//		
+//		String kakaoid = request.getParameter("kakaoemail");
+//		
+//		UserTO userTo = new UserTO();
+		
+		
+		return "/login/loginform";
+	}
+	
+}
