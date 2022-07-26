f903e0530018ea6701a268c44bc27d50e4c39985
jonghyeon1 <dlwhdgus159@gmail.com>
2022-07-26 18:21:00 +0900

    search 검색 수정 -jong

--- src/main/java/com/tech/project_shopping_mall/dao/MainPageDao.java
+++ src/main/java/com/tech/project_shopping_mall/dao/MainPageDao.java
@@ -8,6 +8,11 @@ import com.tech.project_shopping_mall.dto.MainPageSearch_ProductsDto;
 public interface MainPageDao {
 	// 검색해서 다시 받아오는게 2개 이상이라 리스트 형태로 받아야함
 	public ArrayList<MainPageSearchDto> search (String keyword);
+	public ArrayList<MainPageSearchDto> searchtest (String keyword);
+	public void  searchcount(String keyword);
 	public ArrayList<MainPageSearch_ProductsDto> SearchProduct(String keyword_product);
+	public void search_write(String search_word);
+	public void modify(String sbid,String bname,
+			String btitle,String bcontent);
 
 }
