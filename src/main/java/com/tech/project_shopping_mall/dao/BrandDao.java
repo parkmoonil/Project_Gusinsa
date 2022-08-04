260132f107ed355dd84e733707ee17aad47353e6
rlvy0513 <rlvy0513@naver.com>
2022-08-04 23:34:37 +0900

    brand 66

--- src/main/java/com/tech/project_shopping_mall/dao/BrandDao.java
+++ src/main/java/com/tech/project_shopping_mall/dao/BrandDao.java
@@ -9,7 +9,54 @@ public interface BrandDao {
 	
 	public ArrayList<BrandDto> brand_ga(String brand_name);
 	public ArrayList<BrandDto> brand_gga(String brand_name);
-	public BrandDto brand(String brand_name);
+	public ArrayList<BrandDto> brand_na(String brand_name);
+	public ArrayList<BrandDto> brand_da(String brand_name);
+	public ArrayList<BrandDto> brand_dda(String brand_name);
+	public ArrayList<BrandDto> brand_la(String brand_name);
+	public ArrayList<BrandDto> brand_ma(String brand_name);
+	public ArrayList<BrandDto> brand_ba(String brand_name);
+	public ArrayList<BrandDto> brand_bba(String brand_name);
+	public ArrayList<BrandDto> brand_sa(String brand_name);
+	public ArrayList<BrandDto> brand_ssa(String brand_name);
+	public ArrayList<BrandDto> brand_aa(String brand_name);
+	public ArrayList<BrandDto> brand_ja(String brand_name);
+	public ArrayList<BrandDto> brand_jja(String brand_name);
+	public ArrayList<BrandDto> brand_cha(String brand_name);
+	public ArrayList<BrandDto> brand_ka(String brand_name);
+	public ArrayList<BrandDto> brand_ta(String brand_name);
+	public ArrayList<BrandDto> brand_pa(String brand_name);
+	public ArrayList<BrandDto> brand_ha(String brand_name);
+	
+	public ArrayList<BrandDto> brand_a(String brand_name);
+	public ArrayList<BrandDto> brand_b(String brand_name);
+	public ArrayList<BrandDto> brand_c(String brand_name);
+	public ArrayList<BrandDto> brand_d(String brand_name);
+	public ArrayList<BrandDto> brand_e(String brand_name);
+	public ArrayList<BrandDto> brand_f(String brand_name);
+	public ArrayList<BrandDto> brand_g(String brand_name);
+	public ArrayList<BrandDto> brand_h(String brand_name);
+	public ArrayList<BrandDto> brand_i(String brand_name);
+	public ArrayList<BrandDto> brand_j(String brand_name);
+	public ArrayList<BrandDto> brand_k(String brand_name);
+	public ArrayList<BrandDto> brand_l(String brand_name);
+	public ArrayList<BrandDto> brand_m(String brand_name);
+	public ArrayList<BrandDto> brand_n(String brand_name);
+	public ArrayList<BrandDto> brand_o(String brand_name);
+	public ArrayList<BrandDto> brand_p(String brand_name);
+	public ArrayList<BrandDto> brand_q(String brand_name);
+	public ArrayList<BrandDto> brand_r(String brand_name);
+	public ArrayList<BrandDto> brand_s(String brand_name);
+	public ArrayList<BrandDto> brand_t(String brand_name);
+	public ArrayList<BrandDto> brand_u(String brand_name);
+	public ArrayList<BrandDto> brand_v(String brand_name);
+	public ArrayList<BrandDto> brand_w(String brand_name);
+	public ArrayList<BrandDto> brand_x(String brand_name);
+	public ArrayList<BrandDto> brand_y(String brand_name);
+	public ArrayList<BrandDto> brand_z(String brand_name);
+	
+	/* public ArrayList<BrandDto> brand_title(String brand_name); */
+	
+	public BrandDto brand(String brand_id);
 
 	public int selectBoardTotCount();
 	public ArrayList<Infodto> brand1(int rowStart, int rowEnd, String p_img);
--- src/main/java/com/tech/project_shopping_mall/dto/BrandDto.java
+++ src/main/java/com/tech/project_shopping_mall/dto/BrandDto.java
@@ -2,21 +2,31 @@ package com.tech.project_shopping_mall.dto;
 
 public class BrandDto {
 	private String brand_name;
+	private int brand_id;
 	
 	public BrandDto() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public BrandDto(String brand_name) {
+	public BrandDto(String brand_name, int brand_id) {
 		this.brand_name = brand_name;
+		this.brand_id = brand_id;
 	}
 	
 	public String getBrand_name() {
 		return brand_name;
 	}
-	
+
 	public void setBrand_name(String brand_name) {
 		this.brand_name = brand_name;
+	}
+
+	public int getBrand_id() {
+		return brand_id;
+	}
+
+	public void setBrand_id(int brand_id) {
+		this.brand_id = brand_id;
 	}
 	
 }
