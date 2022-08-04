333b3c9ca92f781c62d70ba8f366aa5fa02baf8d
minjootani <tanitanisarang@gmail.com>
2022-08-04 20:26:40 +0900

    결제페이지 생성 -mj

--- src/main/java/com/tech/project_shopping_mall/controller/CategoryController.java
+++ src/main/java/com/tech/project_shopping_mall/controller/CategoryController.java
@@ -81,6 +81,68 @@ public class CategoryController {
         model.addAttribute("men",dto);
 		return "men,women,new/men";
 	}
-	
+	@RequestMapping("/women")
+	public String women(HttpServletRequest request,Model model,SearchVO searchVO) {
+		System.out.println("넘어왔따");
+		PDao dao=sqlSession.getMapper(PDao.class);
+		String p_name=request.getParameter("p_name");
+		String p_price=request.getParameter("p_price");		
+		String p_img=request.getParameter("p_img");
+		
+		String strPage=request.getParameter("page"); //HttpServletRequest request, 가져오기
+		System.out.println("pagggge1 :"+strPage);
+		//null검사
+		if(strPage==null)//처음 리스트에서 list페이지로 넘어갈 때, null임.
+			strPage="1";
+		System.out.println("pagggge2 :"+strPage);
+		int page=Integer.parseInt(strPage);
+		searchVO.setPage(page);
+		
+		//total 글의 갯수 구하기
+//		int total=dao.selectBoardTotCount();
+//		System.out.println("totalrow : "+total);
+		
+		//조건에 따른 갯수 구하기
+		/*
+		 * int total=0; if (p_name.equals("btitle") && bcontent.equals("")) {
+		 * total=dao.selectBoardTotCount1(searchKeyword); }else if (btitle.equals("") &&
+		 * bcontent.equals("bcontent")) { total=dao.selectBoardTotCount2(searchKeyword);
+		 * }else if (btitle.equals("btitle") && bcontent.equals("bcontent")) {
+		 * total=dao.selectBoardTotCount3(searchKeyword); }else if (btitle.equals("") &&
+		 * bcontent.equals("")) { total=dao.selectBoardTotCount4(searchKeyword); }
+		 */
+		
+		int total=dao.selectBoardTotCount();
+		searchVO.pageCalculate(total);
+		
+		//계산된 내용 출력
+		System.out.println("totRow : "+total);
+		System.out.println("clickPage : "+strPage);
+		System.out.println("pageStart : "+searchVO.getPageStart());
+		System.out.println("pageEnd : "+searchVO.getPageEnd());
+		System.out.println("pageTot : "+searchVO.getTotPage());
+		System.out.println("rowStart : "+searchVO.getRowStart());
+		System.out.println("rowEnd : "+searchVO.getRowEnd());
+		
+		int rowStart=searchVO.getRowStart();
+		int rowEnd=searchVO.getRowEnd();
+		
+		/*ArrayList<BoardDto> list=dao.list(rowStart,rowEnd);*/
+//		ArrayList<BoardDto> list=null;
+		
+		
+		
+		ArrayList<Infodto> dto=dao.women(rowStart, rowEnd,"1");
+//		model.addAttribute("list",list);
+		model.addAttribute("totRowcnt",total);
+		model.addAttribute("searchVO",searchVO);
+		model.addAttribute("women",dto);
+		return "men,women,new/women";
+	}
+	@RequestMapping("/Orderafterlogin")
+	public String Orderafterlogin(HttpServletRequest request,Model model,SearchVO searchVO) {
+		PDao dao=sqlSession.getMapper(PDao.class);
+		return "men,women,new/Orderafterlogin";
+	}
 	
 }
--- src/main/java/com/tech/project_shopping_mall/dao/PDao.java
+++ src/main/java/com/tech/project_shopping_mall/dao/PDao.java
@@ -11,4 +11,6 @@ public interface PDao {
 	public ArrayList<Infodto> men(int rowStart, int rowEnd, String p_img);
 
 	public int selectBoardTotCount();
+
+	public ArrayList<Infodto> women(int rowStart, int rowEnd, String string);
 }
