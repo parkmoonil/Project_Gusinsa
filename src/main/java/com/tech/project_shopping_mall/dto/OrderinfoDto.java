4afa6de3878c8962d23edf25b510cc44018422d4
parkmoonil <moonil0213@Naver.com>
2022-08-11 14:58:12 +0900

    병합과정 오류수정

--- src/main/java/com/tech/project_shopping_mall/dto/OrderinfoDto.java
+++ src/main/java/com/tech/project_shopping_mall/dto/OrderinfoDto.java
@@ -3,270 +3,270 @@ package com.tech.project_shopping_mall.dto;
 import java.sql.Date;
 
 public class OrderinfoDto {
-	private int o_code;
-	private int p_code;
-	private String p_name;
-	private int p_price;
-	private String p_img;
-	private String p_color;
-	private String p_size;
-	private int o_count;
-	private int o_totprice;
-	private int o_shipfee;
-	private String maddr_one;
-	private String 	maddr_two;
-	private String maddr_three;
-	private String maddr_four;
-	private String 	mphone;
-	private String 	mid;
-	private String mname;
-	private String mfender;
-	private String 	m_birth;
-	private String o_phone;
-	private String o_add;
-	private String o_quest;
-	private int o_price;
-	private Date o_date;
-	private String 	o_state;
+   private int o_code;
+   private int p_code;
+   private String p_name;
+   private int p_price;
+   private String p_img;
+   private String p_color;
+   private String p_size;
+   private int o_count;
+   private int o_totprice;
+   private int o_shipfee;
+   private String maddr_one;
+   private String    maddr_two;
+   private String maddr_three;
+   private String maddr_four;
+   private String    mphone;
+   private String    mid;
+   private String mname;
+   private String mfender;
+   private String    m_birth;
+   private String o_phone;
+   private String o_add;
+   private String o_quest;
+   private int o_price;
+   private Date o_date;
+   private String    o_state;
 
 public OrderinfoDto() {
-	// TODO Auto-generated constructor stub
+   // TODO Auto-generated constructor stub
 }
 
 
 
 public OrderinfoDto(int o_code, int p_code, String p_name, int p_price, String p_img, String p_color, String p_size,
-		int o_count, int o_totprice, int o_shipfee, String maddr_one, String maddr_two, String maddr_three,
-		String maddr_four, String mphone, String mid, String mname, String mfender, String m_birth, String o_phone,
-		String o_add, String o_quest, int o_price, Date o_date, String o_state) {
-	super();
-	this.o_code = o_code;
-	this.p_code = p_code;
-	this.p_name = p_name;
-	this.p_price = p_price;
-	this.p_img = p_img;
-	this.p_color = p_color;
-	this.p_size = p_size;
-	this.o_count = o_count;
-	this.o_totprice = o_totprice;
-	this.o_shipfee = o_shipfee;
-	this.maddr_one = maddr_one;
-	this.maddr_two = maddr_two;
-	this.maddr_three = maddr_three;
-	this.maddr_four = maddr_four;
-	this.mphone = mphone;
-	this.mid = mid;
-	this.mname = mname;
-	this.mfender = mfender;
-	this.m_birth = m_birth;
-	this.o_phone = o_phone;
-	this.o_add = o_add;
-	this.o_quest = o_quest;
-	this.o_price = o_price;
-	this.o_date = o_date;
-	this.o_state = o_state;
+      int o_count, int o_totprice, int o_shipfee, String maddr_one, String maddr_two, String maddr_three,
+      String maddr_four, String mphone, String mid, String mname, String mfender, String m_birth, String o_phone,
+      String o_add, String o_quest, int o_price, Date o_date, String o_state) {
+   super();
+   this.o_code = o_code;
+   this.p_code = p_code;
+   this.p_name = p_name;
+   this.p_price = p_price;
+   this.p_img = p_img;
+   this.p_color = p_color;
+   this.p_size = p_size;
+   this.o_count = o_count;
+   this.o_totprice = o_totprice;
+   this.o_shipfee = o_shipfee;
+   this.maddr_one = maddr_one;
+   this.maddr_two = maddr_two;
+   this.maddr_three = maddr_three;
+   this.maddr_four = maddr_four;
+   this.mphone = mphone;
+   this.mid = mid;
+   this.mname = mname;
+   this.mfender = mfender;
+   this.m_birth = m_birth;
+   this.o_phone = o_phone;
+   this.o_add = o_add;
+   this.o_quest = o_quest;
+   this.o_price = o_price;
+   this.o_date = o_date;
+   this.o_state = o_state;
 }
 
 
 
 public int getO_code() {
-	return o_code;
+   return o_code;
 }
 
 public void setO_code(int o_code) {
-	this.o_code = o_code;
+   this.o_code = o_code;
 }
 
 public int getP_code() {
-	return p_code;
+   return p_code;
 }
 
 public void setP_code(int p_code) {
-	this.p_code = p_code;
+   this.p_code = p_code;
 }
 
 public String getP_name() {
-	return p_name;
+   return p_name;
 }
 
 public void setP_name(String p_name) {
-	this.p_name = p_name;
+   this.p_name = p_name;
 }
 
 public int getP_price() {
-	return p_price;
+   return p_price;
 }
 
 public void setP_price(int p_price) {
-	this.p_price = p_price;
+   this.p_price = p_price;
 }
 
 public String getP_img() {
-	return p_img;
+   return p_img;
 }
 
 public void setP_img(String p_img) {
-	this.p_img = p_img;
+   this.p_img = p_img;
 }
 
 public String getP_color() {
-	return p_color;
+   return p_color;
 }
 
 public void setP_color(String p_color) {
-	this.p_color = p_color;
+   this.p_color = p_color;
 }
 
 public String getP_size() {
-	return p_size;
+   return p_size;
 }
 
 public void setP_size(String p_size) {
-	this.p_size = p_size;
+   this.p_size = p_size;
 }
 
 public int getO_count() {
-	return o_count;
+   return o_count;
 }
 
 public void setO_count(int o_count) {
-	this.o_count = o_count;
+   this.o_count = o_count;
 }
 
 public int getO_totprice() {
-	return o_totprice;
+   return o_totprice;
 }
 
 public void setO_totprice(int o_totprice) {
-	this.o_totprice = o_totprice;
+   this.o_totprice = o_totprice;
 }
 
 public int getO_shipfee() {
-	return o_shipfee;
+   return o_shipfee;
 }
 
 public void setO_shipfee(int o_shipfee) {
-	this.o_shipfee = o_shipfee;
+   this.o_shipfee = o_shipfee;
 }
 
 public String getMaddr_one() {
-	return maddr_one;
+   return maddr_one;
 }
 
 public void setMaddr_one(String maddr_one) {
-	this.maddr_one = maddr_one;
+   this.maddr_one = maddr_one;
 }
 
 public String getMaddr_two() {
-	return maddr_two;
+   return maddr_two;
 }
 
 public void setMaddr_two(String maddr_two) {
-	this.maddr_two = maddr_two;
+   this.maddr_two = maddr_two;
 }
 
 public String getMaddr_three() {
-	return maddr_three;
+   return maddr_three;
 }
 
 public void setMaddr_three(String maddr_three) {
-	this.maddr_three = maddr_three;
+   this.maddr_three = maddr_three;
 }
 
 public String getMaddr_four() {
-	return maddr_four;
+   return maddr_four;
 }
 
 public void setMaddr_four(String maddr_four) {
-	this.maddr_four = maddr_four;
+   this.maddr_four = maddr_four;
 }
 
 public String getMphone() {
-	return mphone;
+   return mphone;
 }
 
 public void setMphone(String mphone) {
-	this.mphone = mphone;
+   this.mphone = mphone;
 }
 
 public String getMid() {
-	return mid;
+   return mid;
 }
 
 public void setMid(String mid) {
-	this.mid = mid;
+   this.mid = mid;
 }
 
 public String getMname() {
-	return mname;
+   return mname;
 }
 
 public void setMname(String mname) {
-	this.mname = mname;
+   this.mname = mname;
 }
 
 public String getMfender() {
-	return mfender;
+   return mfender;
 }
 
 public void setMfender(String mfender) {
-	this.mfender = mfender;
+   this.mfender = mfender;
 }
 
 public String getM_birth() {
-	return m_birth;
+   return m_birth;
 }
 
 public void setM_birth(String m_birth) {
-	this.m_birth = m_birth;
+   this.m_birth = m_birth;
 }
 
 public String getO_phone() {
-	return o_phone;
+   return o_phone;
 }
 
 public void setO_phone(String o_phone) {
-	this.o_phone = o_phone;
+   this.o_phone = o_phone;
 }
 
 public String getO_add() {
-	return o_add;
+   return o_add;
 }
 
 public void setO_add(String o_add) {
-	this.o_add = o_add;
+   this.o_add = o_add;
 }
 
 public String getO_quest() {
-	return o_quest;
+   return o_quest;
 }
 
 public void setO_quest(String o_quest) {
-	this.o_quest = o_quest;
+   this.o_quest = o_quest;
 }
 
 public int getO_price() {
-	return o_price;
+   return o_price;
 }
 
 public void setO_price(int o_price) {
-	this.o_price = o_price;
+   this.o_price = o_price;
 }
 
 public Date getO_date() {
-	return o_date;
+   return o_date;
 }
 
 public void setO_date(Date o_date) {
-	this.o_date = o_date;
+   this.o_date = o_date;
 }
 
 public String getO_state() {
-	return o_state;
+   return o_state;
 }
 
 public void setO_state(String o_state) {
-	this.o_state = o_state;
+   this.o_state = o_state;
 }
 
 
