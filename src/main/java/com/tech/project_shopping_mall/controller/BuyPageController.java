687a243d44f81daa13fa5c69b56b4c661fc9ab74
SunnyeyKim <k0170@nate.com>
2022-08-14 14:42:35 +0900

    pull key 220814 1차

--- src/main/java/com/tech/project_shopping_mall/controller/BuyPageController.java
+++ src/main/java/com/tech/project_shopping_mall/controller/BuyPageController.java
@@ -3,7 +3,6 @@ package com.tech.project_shopping_mall.controller;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,38 +12,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tech.project_shopping_mall.dao.MainPageDao;
 import com.tech.project_shopping_mall.dto.Infodto;
-import com.tech.project_shopping_mall.dto.StorageDto;
 
 @Controller
 public class BuyPageController {
 
-	@Autowired
-	private SqlSession sqlSession;
-	
-	
-	@RequestMapping("BuyPage")
-	public String BuyPage(HttpServletRequest request, Model model) {
-		
-		HttpSession session = request.getSession();
-		String mid = (String )session.getAttribute("mid");
-		
-		
-		
-		
-		
-		
-		MainPageDao dao = sqlSession.getMapper(MainPageDao.class);
-		int pcode = Integer.parseInt(request.getParameter("p_code"));
-		//String pcode = request.getParameter("p_code");
+   @Autowired
+   private SqlSession sqlSession;
+   
+   
+   @RequestMapping("BuyPage")
+   public String BuyPage(HttpServletRequest request, Model model) {
+      
+      MainPageDao dao = sqlSession.getMapper(MainPageDao.class);
+      int pcode = Integer.parseInt(request.getParameter("p_code"));
+      //String pcode = request.getParameter("p_code");
 
-		StorageDto strgdto= dao.storage(pcode);
         Infodto indto = dao.search_prouct_detail(pcode);
-		System.out.println("indto : " + indto);
-		
-		model.addAttribute("strgdto",strgdto);
-		model.addAttribute("indto", indto);
-		return "/Sale/SuperSale";
-		
-	}
-	
+      System.out.println("indto : " + indto);
+
+      model.addAttribute("indto", indto);
+      return "/Sale/SaleBuying";
+      
+   }
+   
+   
+   @RequestMapping("SaleBuyPage")
+   public String SaleBuyPage(HttpServletRequest request, Model model) {
+      
+      MainPageDao dao = sqlSession.getMapper(MainPageDao.class);
+      int pcode = Integer.parseInt(request.getParameter("p_code"));
+      //String pcode = request.getParameter("p_code");
+
+        Infodto indto = dao.search_prouct_detail(pcode);
+      System.out.println("indto : " + indto);
+
+      model.addAttribute("indto", indto);
+      return "/Sale/SuperSale";
+      
+   }
 }
