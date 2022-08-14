11875856e66a8f2428025db9aef6acd1e98f466d
jonghyeon1 <dlwhdgus159@gmail.com>
2022-08-14 18:21:17 +0900

    dao.수정

--- src/main/java/com/tech/project_shopping_mall/dao/MainPageDao.java
+++ src/main/java/com/tech/project_shopping_mall/dao/MainPageDao.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import com.tech.project_shopping_mall.dto.Infodto;
 import com.tech.project_shopping_mall.dto.MainPageSearchDto;
 import com.tech.project_shopping_mall.dto.MainPageSearch_ProductsDto;
+import com.tech.project_shopping_mall.dto.ReviewDto;
 import com.tech.project_shopping_mall.dto.StorageDto;
 
 public interface MainPageDao {
@@ -28,8 +29,18 @@ public interface MainPageDao {
 	
 	
 	public Infodto search_prouct_detail(int pcode);
+//재고테이블
 	public ArrayList<StorageDto> storage(int pcode);
-
+//리뷰
+	// 리뷰
+	public ArrayList<ReviewDto> review_list(int rowStart, int rowEnd);
+	public void review_write(int pcode, String mid, String r_starpoint ,String r_title, String r_contents, String fname, int ocode);
+	public ReviewDto review_view(String r_num);
+	public void review_modify(String r_num,String r_starpoint, String r_title,String r_contents);
+	public void review_delete(String r_num);
+	public void download(String path, String fname, String r_num);   
+	public double staravg(int pcode);
+	public int selectReviewTotCount();
 
 	
 }
